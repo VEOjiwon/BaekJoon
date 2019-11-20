603c2a702cec23d1b5409666e7c23119be601be8
gimjiwon <top9076@naver.com>
2019-11-21 01:56:34 +0900

    곱셈문제까지 해서 입출력과 사칙연산 완료

--- Day1_printcat.py
+++ Day1_printcat.py
@@ -7,7 +7,7 @@ A*B
 A/B
 사칙연산
 나머지
-
+곱셈 2588번문제
 '''
 
 
@@ -24,6 +24,7 @@ print('''|\_/|
 ||_/=\\\\__|''')
 
 '''
+#사칙연산
 L = list(map(int, input().split()))
 
 print(L[0]+L[1])
@@ -31,9 +32,19 @@ print(L[0]-L[1])
 print(L[0]*L[1])
 print(int(L[0]/L[1]))
 print(L[0]%L[1])
-'''
+
+#나머지 
 L = list(map(int, input().split()))
 print((L[0]+L[1])%L[2])
 print((L[0]%L[2]+L[1]%L[2])%L[2])
 print((L[0]*L[1])%L[2])
 print(((L[0]%L[2])*(L[1]%L[2]))%L[2])
+'''
+
+#곱셈문제
+a = int(input())
+b =  int(input())
+print((b%10)*a)
+print(int(b%100*0.1)*a)
+print(int(b*0.01)*a)
+print(a*b)
