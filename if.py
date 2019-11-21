0faa39197456ed6b73a5e987e6b903f9028ec81c
gimjiwon <top9076@naver.com>
2019-11-21 16:56:19 +0900

    if문 2번문제해결

--- if.py
+++ if.py
@@ -3,7 +3,7 @@
 if문 문제
 
 """
-
+"""
 #1번(1330번) 두 수 비교하기
 a = list(map(int, input().split()))
 
@@ -13,3 +13,18 @@ elif a[0]<a[1] :
     print("<")
 else :
     print("==")
+
+"""
+#2번(9498번)
+score = int(input())
+
+if 90 <= score <= 100 :
+    print("A")
+elif 80 <= score <90:
+    print("B")
+elif 70 <= score < 80:
+    print("C")
+elif 60 <= score < 70:
+    print("D")
+else:
+    print("F")
