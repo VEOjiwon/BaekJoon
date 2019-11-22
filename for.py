a3b5ec6127bb1ea98e800f3b43c76dfc96de0707
gimjiwon <top9076@naver.com>
2019-11-22 14:49:11 +0900

    for문 3번 1부터 n가지 합 구하기

--- for.py
+++ for.py
@@ -5,17 +5,26 @@
 #구구단(2739번) 입력받은 수의 구구단 출력하기
 
 num = int(input())
-
-
 for i in range(1,10):
     print(num,"*",i,"=",num*i)
-"""
+
+#########################################################
 
 #2번 a+b-3
-
 num = int(input())
 for i in range(0,num):
     a, b = input().split()
     a = int(a)
     b = int(b)
     print(a+b)
+    
+############################################################
+"""
+
+#3번 1부터 n까지 합 구하
+
+x = int(input())
+output =0;
+for i in range(1,x+1):
+    output=output+i
+print(output)
