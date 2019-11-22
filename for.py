ce49ead60cc7f5e4bb98c7d7601ca79047eaa427
gimjiwon <top9076@naver.com>
2019-11-22 14:45:32 +0900

    for문 2번 a+b-3

--- for.py
+++ for.py
@@ -1,7 +1,7 @@
 """
 백준 for
 """
-
+""""
 #구구단(2739번) 입력받은 수의 구구단 출력하기
 
 num = int(input())
@@ -9,3 +9,13 @@ num = int(input())
 
 for i in range(1,10):
     print(num,"*",i,"=",num*i)
+"""
+
+#2번 a+b-3
+
+num = int(input())
+for i in range(0,num):
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    print(a+b)
