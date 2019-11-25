2d0dfe76ddf2324a51f42fd949c3a1d5aa30fb1a
VEOjiwon <cnu.ice.k@gmail.com>
2019-11-25 14:44:42 +0900

    A+B-4

--- while.py
+++ while.py
@@ -1,10 +1,11 @@
 """
 while문
 """
-import sys
+
 
 """
 #A + B - 5
+import sys
 while True:
     l=list(map(int,sys.stdin.readline().rstrip().split()))
     if l[0] and l[1] !=0 :
@@ -14,4 +15,13 @@ while True:
         break
 ###############################################
 """
+# A+B - 4
+import sys
+while True:
+    l=list(map(int,sys.stdin.readline().rstrip().split()))
+    if not l:
+        break
+    print(l[0]+l[1])
+
+
 
