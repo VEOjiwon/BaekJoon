1a29ed429ffe8cfaedc1dcffb0546b3d7dfb9ee9
VEOjiwon <cnu.ice.k@gmail.com>
2019-11-26 04:20:00 +0900

    음계문제 해결

--- one_array.py
+++ one_array.py
@@ -19,11 +19,11 @@ for i in l:
         min=i
 
 print(min,max)
-"""
 
-#최대값
+###############################################33
 
 
+#최대값구하는 문제 
 l = list()
 cnt = 1
 max_cnt=0
@@ -40,3 +40,23 @@ for i in range(0,9):
         max = l[i]
     cnt+=1
 print(max, max_cnt,sep="\n")
+"""
+
+#음계
+import sys
+l = list(map(int,sys.stdin.readline().split()))
+asccnt=0
+descnt=0
+
+for i in range(0,7):
+    if(l[i]<l[i+1]):
+        asccnt+=1
+    elif(l[i]>l[i+1]):
+        descnt+=1
+
+if asccnt==7:
+    print("ascending")
+elif descnt==7:
+    print("descending")
+else:
+    print("mixed")
