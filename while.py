05534644990dcefcd0468c8119601a1cf303af81
VEOjiwon <cnu.ice.k@gmail.com>
2019-11-25 14:35:10 +0900

    A + B - 5

---
+++ while.py
@@ -0,0 +1,17 @@
+"""
+while문
+"""
+import sys
+
+"""
+#A + B - 5
+while True:
+    l=list(map(int,sys.stdin.readline().rstrip().split()))
+    if l[0] and l[1] !=0 :
+        print(l[0]+l[1])
+        continue
+    else:
+        break
+###############################################
+"""
+
