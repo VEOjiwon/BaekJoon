7eebaf973ee60a996fc5d3177f5392c8944ffff3
VEOjiwon <cnu.ice.k@gmail.com>
2019-11-25 15:19:00 +0900

    더하기 싸이클 while 끝

--- while.py
+++ while.py
@@ -14,7 +14,7 @@ while True:
     else:
         break
 ###############################################
-"""
+
 # A+B - 4
 import sys
 while True:
@@ -23,5 +23,33 @@ while True:
         break
     print(l[0]+l[1])
 
+###############################################
+"""
 
+#더하기 싸이클
+target = int(input())
+temp = target
+cnt = 0
+while True:
+    if int(temp / 10) ==0:
+        first_num=temp
+        second_num=0
+    else :
+        first_num = temp %10
+        second_num = int(temp /10)
+    sumof_fs = first_num + second_num
+    #print(first_num,second_num,sumof_fs)
+    if sumof_fs>10:
+        new_num = int(sumof_fs%10) + first_num*10
+    elif sumof_fs == 10:
+        new_num = first_num * 10
+    else:
+        new_num = sumof_fs + first_num * 10
+    #print(new_num)
+    cnt+=1
 
+    if new_num == target:
+        print(cnt)
+        break
+    else:
+        temp = new_num
