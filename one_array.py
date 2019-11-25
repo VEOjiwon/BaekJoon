145b6ae6ac33c2d7754080a3d1f113954ab8150e
VEOjiwon <cnu.ice.k@gmail.com>
2019-11-26 04:05:33 +0900

    최대값

--- one_array.py
+++ one_array.py
@@ -1,7 +1,11 @@
 """
 1 차원 배열
 """
+import sys
 
+from pip._vendor.distlib.compat import raw_input
+
+"""
 #최대 최소값
 import sys
 length = int(sys.stdin.readline())
@@ -15,3 +19,24 @@ for i in l:
         min=i
 
 print(min,max)
+"""
+
+#최대값
+
+
+l = list()
+cnt = 1
+max_cnt=0
+max=0
+
+#입력부
+for i in range(0,9):
+    l.append(int(input()))
+
+#최대값 비교부
+for i in range(0,9):
+    if max < l[i]:
+        max_cnt=cnt
+        max = l[i]
+    cnt+=1
+print(max, max_cnt,sep="\n")
