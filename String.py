cf6b18980a446c71464f7476d7436b5e3dbe7cc0
VEOjiwon <cnu.ice.k@gmail.com>
2019-12-01 05:37:37 +0900

    숫자의 합

--- String.py
+++ String.py
@@ -1,10 +1,19 @@
 """
 백준 문자열문제 모음
 """
-
+"""
 #아스키코드 변환
 target = input()
 print(ord(target))
 
+################################
+"""
 
+#숫자의 합
+numbers = int(input())
+target = input()
+sum=0
+for i in target:
+    sum+=int(i)
 
+print(sum)
