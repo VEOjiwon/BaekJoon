e954d94c79fedb6e888e8f375af9e836877b7d3e
VEOjiwon <cnu.ice.k@gmail.com>
2019-12-09 12:55:36 +0900

    math part1 start : sugar delivery solve!!

--- Math_1.py
+++ Math_1.py
@@ -7,3 +7,29 @@ if b<c:
     cnt=int(a/k+1)
     print(cnt)
 else:print("-1")
+
+#sugar delivery
+target = int(input())
+
+m5 = target//5
+mod5 = target % 5
+cnt=0
+
+cant = [4,7]
+if target in cant:
+    cnt = -1
+else :
+    if mod5 == 0:
+        cnt = m5
+    elif  (mod5 ==1) or (mod5==4) :
+        tmp = m5-1
+        tmp3 = target-(5*tmp)
+        cnt=tmp3//3+tmp
+    elif mod5 == 2 :
+        tmp = m5-2
+        tmp3 = target-(5*tmp)
+        cnt=tmp3//3+tmp
+    elif mod5 ==3:
+        cnt = mod5//3 + m5
+
+print(cnt)
