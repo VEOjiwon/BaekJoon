0b1e92bb706cc4944e0227551e8e278fe857b6a6
VEOjiwon <cnu.ice.k@gmail.com>
2019-12-19 00:23:31 +0900

    소수 2581 해결

--- Math2.py
+++ Math2.py
@@ -11,3 +11,26 @@ for i in l:
             cnt-=1
             break
 print(cnt)
+
+#prime problem #2581
+def is_prime(i):
+    if i==1:
+        return 0
+    for k in range(2,i):
+        if(i%k==0):
+            return 0
+            break
+    return 1
+
+m = int(input())
+n = int(input())
+
+l = []
+for i in range(m,n+1):
+    if is_prime(i):
+        l.append(i)
+if len(l) == 0:
+    print('-1')
+else:
+    print(sum(l))
+    print(min(l))
