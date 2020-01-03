12dd02061e340cbf20ed2e7dc2797c200c5131a7
VEOjiwon <cnu.ice.k@gmail.com>
2020-01-03 23:20:10 +0900

    stack 2번 성공

--- stack.py
+++ stack.py
@@ -44,3 +44,50 @@ for i in range(rept):
         print(empty())
     if tar[0]=="top":
         print(top())
+
+###########################################################################
+
+#제로
+import sys
+
+# stack
+
+sta=[]
+def push(x):
+    global sta
+    sta.append(x)
+
+def pop():
+    global sta
+    if len(sta)==0:
+        return -1
+    else:
+        return sta.pop()
+def size():
+    global sta
+    return len(sta)
+
+def empty():
+    global sta
+    if len(sta)==0:
+        return 1
+    else: return 0
+
+def top():
+    global sta
+    if len(sta)==0: return -1
+    else : return sta[-1]
+
+
+
+rept = int(input())
+
+for i in range(rept):
+    tar = sys.stdin.readline()
+    tar=int(tar)
+    if tar==0:
+        x=pop()
+    else:
+        push(tar)
+
+print(sum(sta))
